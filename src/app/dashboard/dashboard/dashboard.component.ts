79679f8dcabd35cea58cee8b0710190fab8bd873
Sebas <sebas@MacBook-Air-de-Sebas.local>
2024-07-02 20:37:36 -0500

    link-01

--- src/app/dashboard/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard/dashboard.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HomeComponent, RouterOutlet],
+  imports: [HomeComponent, RouterOutlet, RouterLink],
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
