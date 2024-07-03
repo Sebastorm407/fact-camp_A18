41c01fea8661e097f7a37f37eb0f60fdee7dd3f7
Sebastian <asvc36@gmail.com>
2024-07-03 18:08:30 -0500

    routerLink-02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardComponent, RouterLink],
+  imports: [RouterOutlet, DashboardComponent, RouterLink, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,36 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './dashboard/dashboard/home/home.component';
-import { ProductComponent } from './dashboard/dashboard/product/product.component';
-import { GraphicComponent } from './dashboard/dashboard/graphic/graphic.component';
-import { ReportComponent } from './dashboard/dashboard/report/report.component';
-import { EmployeeComponent } from './dashboard/dashboard/employee/employee.component';
-import { ClientComponent } from './dashboard/dashboard/client/client.component';
-import { SupplierComponent } from './dashboard/dashboard/client/supplier/supplier.component';
-import { SupplyComponent } from './dashboard/dashboard/product/supply/supply.component';
-import { BuyerComponent } from './dashboard/dashboard/client/buyer/buyer.component';
-import { FactComponent } from './dashboard/dashboard/fact/fact.component';
-import { AddProductComponent } from './dashboard/dashboard/product/add-product/add-product.component';
-import { AddSupplyComponent } from './dashboard/dashboard/product/add-supply/add-supply.component';
+import { HomeComponent } from './dashboard/pages/home/home.component';
+import { ProductComponent } from './dashboard/pages/product/product.component';
+import { GraphicComponent } from './dashboard/pages/graphic/graphic.component';
+import { ReportComponent } from './dashboard/pages/report/report.component';
+import { EmployeeComponent } from './dashboard/pages/employee/employee.component';
+import { ClientComponent } from './dashboard/pages/client/client.component';
+import { SupplierComponent } from './dashboard/pages/client/supplier/supplier.component';
+import { SupplyComponent } from './dashboard/pages/product/supply/supply.component';
+import { BuyerComponent } from './dashboard/pages/client/buyer/buyer.component';
+import { FactComponent } from './dashboard/pages/fact/fact.component';
+import { AddProductComponent } from './dashboard/pages/product/add-product/add-product.component';
+import { AddSupplyComponent } from './dashboard/pages/product/add-supply/add-supply.component';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
 export const routes: Routes = [
-  {path: 'home', component: HomeComponent},
-  {path: 'product', component: ProductComponent},
-  {path: 'graphic', component: GraphicComponent},
-  {path: 'report', component: ReportComponent},
-  {path: 'employee', component: EmployeeComponent},
-  {path: 'client', component: ClientComponent},
-  {path: 'supplier', component: SupplierComponent},
-  {path: 'supply', component: SupplyComponent},
-  {path: 'buyer', component: BuyerComponent},
-  {path: 'fact', component: FactComponent},
-  {path: 'add-product', component: AddProductComponent},
-  {path: 'add-supply', component: AddSupplyComponent}
+  {path: '', component: LoginComponent},
+  {path: 'dashboard', component: DashboardComponent, children: [
+    {path: 'home', component: HomeComponent},
+    {path: 'product', component: ProductComponent, children: [
+      {path: 'add-product', component: AddProductComponent},
+      {path: 'add-supply', component: AddSupplyComponent}
+    ]},
+    {path: 'graphic', component: GraphicComponent},
+    {path: 'report', component: ReportComponent},
+    {path: 'employee', component: EmployeeComponent},
+    {path: 'client', component: ClientComponent},
+    {path: 'supplier', component: SupplierComponent},
+    {path: 'supply', component: SupplyComponent},
+    {path: 'buyer', component: BuyerComponent},
+    {path: 'fact', component: FactComponent},
+
+  ]},
+
 ];
